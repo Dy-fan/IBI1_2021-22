ef1985de4ee294be7a810f23072de6113dd683ba
Deng <yifan.21@intl.zju.edu.cn>
2022-03-22 10:07:55 +0100

    add Practical6

---
+++ Practical6/List manipulation.py
@@ -0,0 +1,32 @@
+# create a list called marks to record all the marks and sort the list in ascending order
+# draw a boxplot using matplotlib module
+# explore some options to make the boxplot look better
+# calculate the average and compare it with 60 to see whether he passes
+import matplotlib.pyplot as plt  # import matplotlib module in python for plotting
+
+marks = [45, 36, 86, 57, 53, 92, 65, 45]
+marks.sort()  # sort the list in ascending order
+print(marks)
+
+# draw the boxplot of the marks
+# put the median red, the box cyan and the edge black
+plt.boxplot(marks,
+            medianprops={'color': 'red'},
+            boxprops={'facecolor': 'cyan', 'edgecolor': 'black'},
+            patch_artist=True)
+plt.title('boxplot for marks of Rob')  # add the boxplot a title
+plt.show()
+
+# calculate the average of the 8 marks using loop
+total = 0
+for i in marks:
+    total += i
+average = total / len(marks)
+print('The average mark Rob has received across the eight practicals is', str(average))
+# determine whether the average is greater than 60 or not so to decide Rob passed or not
+if average < 60:
+    print("It's lower than the pass mark of 60%")
+    print('Rob has not passed')
+else:
+    print("It's higher than the pass mark of 60%")
+    print('Rob has passed')
---
+++ Practical6/Parental age vs offspring health.py
@@ -0,0 +1,29 @@
+# input and Turn list paternal_age and list chd into a dictionary
+# let the values in the same position in each list correspond to each other
+# draw a scatter plot use matplotlib module
+# print the corresponding risk rate for a certain age input by the user
+import matplotlib.pyplot as plt  # import matplotlib module in python for plotting
+
+# (1)
+paternal_age = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75]  # create two list recording the input data
+chd = [1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94]
+# the code in the next line was obtained from https://stackoverflow.com/questions/44374540/cant-merge
+# -two-lists-into-a-dictionary on 22/03/22
+# This Python builtin zip() returns an iterable of tuples from each of the arguments.
+# Then give this to the dict() create a dictionary where each of the items in paternal_age is the key
+# and items in chd is the corresponding value.
+dictionary = dict(zip(paternal_age, chd))
+print(dictionary)
+
+# (2)
+plt.scatter(paternal_age, chd, marker='o', color='blue') # draw a scatter plot with blue dot
+plt.xticks(paternal_age)  # set label locations with the items in the list input
+plt.yticks(chd)
+plt.grid()  # add a grid in the background
+plt.show()
+
+# (3)
+age = int(input('the age you want to search:'))  # where the user can input the age they want to check
+# here turn age into int since the keys in the dictionary are all int
+risk = dictionary[age]  # find the certain risk rate according to the age input
+print('the relative risk of congenital disease for '+str(age)+'-year-old father is '+str(risk))
